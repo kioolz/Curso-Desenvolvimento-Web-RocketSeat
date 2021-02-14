a72c147fe07c541b6e3fc87c97e957c93fbedb36
kioolz <caiovorb625@gmail.com>
2021-02-14 11:17:32 -0300

    Delete index.js

--- index.js
+++
@@ -1,5 +0,0 @@
-//Criar uma variável
-//O Curso do gustavo guanabara me ensinou como fazer os comentários em JS
-// Isso me lembra até que um exercício interessante que ele passou é
-// Como diferenciar os comandos de comentário em JS, CSS e HTML.
-const nome = 'Caio'
