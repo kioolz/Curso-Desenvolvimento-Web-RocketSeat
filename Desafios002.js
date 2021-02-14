a3c3986fdb80f3de4f5710e1a297a23309ac53ec
kioolz <caiovorb625@gmail.com>
2021-02-14 20:15:23 -0300

    Create Desafios002.js

---
+++ Desafios002.js
@@ -0,0 +1,30 @@
+/*
+
+Desafios para fortalecer alguns conceitos, entre eles:
+
+Objetos;
+Vetores.
+Construção e impressão de objetos
+Crie um programa que armazena dados da empresa Rocketseat dentro de um objeto chamado empresa. Os dados a serem armazenados são:
+
+Nome: Rocketseat
+
+Cor: Roxo
+
+Foco: Programação
+
+Endereço:
+
+Rua: Rua Guilherme Gembala
+Número: 260
+*/
+
+const usuario = {
+    nome: "Caio",
+    empresa: {
+        nome: "Torta de pamonha"
+
+    }
+};
+
+console.log(`A empresa ${usuario.empresa} é legal`)
